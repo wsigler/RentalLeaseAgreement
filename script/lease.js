33da9a544b838f6ebc9f6546f2ed6b6d644b8334
wsigler <sigler.properties@gmail.com>
2015-06-24 21:02:59 -0500

    Refactored and made lease driven by firebase

--- script/lease.js
+++ script/lease.js
@@ -1,6 +1,38 @@
 $(document).ready(function() {
-    // Get a database reference to our posts
-	var ref = new Firebase("https://intense-heat-8777.firebaseio.com/LeaseForm/LeaseInfo");
+	var sects = new Firebase("https://intense-heat-8777.firebaseio.com/LeaseForm");
+    //Populating the sections of the lease
+    sects.child("Sections").on("value", function(sections){
+		var counter = 0;
+	    sections.forEach(function(section) {
+	      	var key = section.key();
+	        counter++;
+	        PopulateSections("Section" + counter, "headSection" + counter, "section" + counter + "Text");
+	    });
+    });
+    var ref = new Firebase("https://intense-heat-8777.firebaseio.com/LeaseForm/LeaseInfo");
+	SetTenantInfo(ref);
+    SetPropertyInfo(ref);
+    SetDateInfo(ref);
+    SetAmountInfo(ref);
+});
+
+function PopulateSections(section, headerId, blurbId){
+	var sections = new Firebase("https://intense-heat-8777.firebaseio.com/LeaseForm/Sections/" + section);
+	
+	sections.child("Header").on("value", function(section){
+		$('#' + headerId).html(function() {
+	    	return $(this).text().replace('[headSection]', section.val());
+		});
+	});
+
+	sections.child("Blurb").on("value", function(section){
+		$('#' + blurbId).html(function() {
+	    	return $(this).text().replace('[section]', section.val());
+		});
+	});
+}
+
+function SetTenantInfo(ref){
 	ref.child("PrimaryTenant").on("value", function(primaryTenant){
 		$('#section1Text').html(function() {
 	    	return $(this).text().replace('[primaryTenant]', primaryTenant.val());
@@ -46,42 +78,6 @@ $(document).ready(function() {
 		});
 	});
 
-	ref.child("Property").on("value", function(property){
-		$('#section1Text').html(function() {
-	    	return $(this).text().replace('[property]', property.val());
-		});
-	});
-	
-	ref.child("LeaseStartDate").on("value", function(leaseStartDate){
-		$('#section1Text').html(function() {
-	    	return $(this).text().replace('[leaseStartDate]', leaseStartDate.val());
-		});
-	});
-
-	ref.child("LeaseEndDate").on("value", function(leaseEndDate){
-		$('#section1Text').html(function() {
-	    	return $(this).text().replace('[leaseEndDate]', leaseEndDate.val());
-		});
-	});
-
-	ref.child("RentAmount").on("value", function(rentAmount){
-		$('#section1Text').html(function() {
-	    	return $(this).text().replace('[rentAmount]', rentAmount.val());
-		});
-	});
-	
-	ref.child("DepositAmount").on("value", function(deposit){
-		$('#section1Text').html(function() {
-	    	return $(this).text().replace('[deposit]', deposit.val());
-		});
-	});
-
-	ref.child("DepositDate").on("value", function(depositDate){
-		$('#section1Text').html(function() {
-	    	return $(this).text().replace('[depositDate]', depositDate.val());
-		});
-	});
-
 	ref.child("NumbersAdult").on("value", function(numbersAdult){
 		$('#section3Text li').html(function() {
 	    	return $(this).text().replace('[numbersAdult]', numbersAdult.val());
@@ -94,9 +90,53 @@ $(document).ready(function() {
 		});
 	});
 
+}
+
+function SetPropertyInfo(ref){
+    ref.child("Property").on("value", function(property){
+		$('#section1Text').html(function() {
+	    	return $(this).text().replace('[property]', property.val());
+		});
+	});
+
 	ref.child("TrashPickup").on("value", function(trashPickup){
 		$('#section6Text').html(function() {
 	    	return $(this).text().replace('[trashPickup]', trashPickup.val());
+		});
+	});
+}
+
+function SetDateInfo(ref){
+    ref.child("LeaseStartDate").on("value", function(leaseStartDate){
+		$('#section1Text').html(function() {
+	    	return $(this).text().replace('[leaseStartDate]', leaseStartDate.val());
+		});
+	});
+
+	ref.child("LeaseEndDate").on("value", function(leaseEndDate){
+		$('#section1Text').html(function() {
+	    	return $(this).text().replace('[leaseEndDate]', leaseEndDate.val());
+		});
+	});
+
+	ref.child("DepositDate").on("value", function(depositDate){
+		$('#section1Text').html(function() {
+	    	return $(this).text().replace('[depositDate]', depositDate.val());
+		});
+	});
+
+}
+
+function SetAmountInfo(ref){
+	ref.child("RentAmount").on("value", function(rentAmount){
+		$('#section1Text').html(function() {
+	    	return $(this).text().replace('[rentAmount]', rentAmount.val());
+		});
+	});
+	
+	ref.child("DepositAmount").on("value", function(deposit){
+		$('#section1Text').html(function() {
+	    	return $(this).text().replace('[deposit]', deposit.val());
 		});
 	});
 
@@ -105,4 +145,4 @@ $(document).ready(function() {
 	    	return $(this).text().replace('[petDeposit]', petDeposit.val());
 		});
 	});
-});
+}
